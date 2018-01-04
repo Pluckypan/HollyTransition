7fcdbbe3630a753791e1a1a4a34b5f820bd0cae7
plucky <py@meitu.com>
2018-01-04 17:02:21 +0800

    实现路径动画

--- app/src/main/java/engineer/echo/transition/fragment/AppCtrlFragment.java
+++ app/src/main/java/engineer/echo/transition/fragment/AppCtrlFragment.java
@@ -2,14 +2,28 @@ package engineer.echo.transition.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.ViewCompat;
+import android.transition.ArcMotion;
+import android.transition.ChangeBounds;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import engineer.echo.transition.Constants;
 import engineer.echo.transition.R;
-import engineer.echo.transition.context.BaseFragment;
+import engineer.echo.transition.cmpts.context.BaseFragment;
+import engineer.echo.transition.cmpts.events.CaptureFinishEvent;
+import engineer.echo.transition.cmpts.utils.CommonUtil;
+import engineer.echo.transition.cmpts.widget.reflect.ReflectItemView;
 
 /**
  * AppCtrlFragment
@@ -25,9 +39,14 @@ public class AppCtrlFragment extends BaseFragment implements View.OnClickListene
     public static String SHARE_NAME_RIGHT = "share_element_of_right";
     private View mSceneBtn, mPhotoBtn, mSettingsBtn;
     private Pair<View, String>[] mShareViews = new Pair[3];
+    private ReflectItemView mCaptureHolder;
+    private ImageView mCaptureView;
+    private ConstraintLayout mRootView;
+    private int mEndSize, mMargin;
 
     public AppCtrlFragment() {
-
+        mEndSize = (int) CommonUtil.dip2px(50);
+        mMargin = (int) CommonUtil.dip2px(30);
     }
 
     public static AppCtrlFragment newInstance() {
@@ -58,10 +77,15 @@ public class AppCtrlFragment extends BaseFragment implements View.OnClickListene
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        EventBus.getDefault().register(this);
         View view = inflater.inflate(R.layout.fragment_ctrl, container, false);
+
+        mRootView = view.findViewById(R.id.root_view);
         mSceneBtn = view.findViewById(R.id.scene_btn);
         mPhotoBtn = view.findViewById(R.id.take_photo_btn);
         mSettingsBtn = view.findViewById(R.id.settings_btn);
+        mCaptureHolder = view.findViewById(R.id.capture_holder);
+        mCaptureView = view.findViewById(R.id.capture_image);
 
         mSceneBtn.setOnClickListener(this);
         mPhotoBtn.setOnClickListener(this);
@@ -80,6 +104,39 @@ public class AppCtrlFragment extends BaseFragment implements View.OnClickListene
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        EventBus.getDefault().unregister(this);
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onCaptureFinishEvent(CaptureFinishEvent event) {
+        Log.d(TAG, "onCaptureFinishEvent: ");
+        mCaptureView.setImageResource(R.drawable.girl_4);
+
+        mCaptureHolder.setAlpha(1.0f);
+
+        ChangeBounds changeBounds = new ChangeBounds();
+        changeBounds.setPathMotion(new ArcMotion());
+        changeBounds.setDuration(Constants.TRANSITION_TIME);
+        TransitionManager.beginDelayedTransition(mRootView,
+                changeBounds);
+
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) mCaptureHolder.getLayoutParams();
+        params.width = mEndSize;
+        params.height = mEndSize;
+
+        params.endToEnd = -1;
+        params.startToStart = -1;
+        params.topToTop = -1;
+
+        params.bottomToBottom = R.id.root_view;
+        params.endToStart = R.id.take_photo_btn;
+
+        params.bottomMargin = mMargin;
+        params.rightMargin = mMargin;
+        params.topMargin = 0;
+
+        mCaptureHolder.setLayoutParams(params);
     }
 
     @Override
@@ -88,6 +145,9 @@ public class AppCtrlFragment extends BaseFragment implements View.OnClickListene
             case R.id.scene_btn:
                 break;
             case R.id.take_photo_btn:
+                //拍照很卡 暂时屏蔽
+                //EventBus.getDefault().post(new StartCaptureEvent());
+                onCaptureFinishEvent(null);
                 break;
             case R.id.settings_btn:
                 SettingsFragment.gotoPage(getFragmentManager(), mShareViews);
