8c00d29eab6f5f16c0d9d2870f017a45fa2ab372
plucky <py@meitu.com>
2020-04-08 14:59:38 +0800

    [feature] remove useless code

--- app/src/main/java/engineer/echo/transition/cmpts/widget/transition/SafeSlide.java
+++ app/src/main/java/engineer/echo/transition/cmpts/widget/transition/SafeSlide.java
@@ -4,12 +4,6 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.transition.Slide;
 
-/**
- * SafeSlide.java
- * Useage: SafeSlide
- * Created by Plucky<py@meitu.com> on 2018/1/8 - 11:17
- */
-
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class SafeSlide extends Slide {
 
